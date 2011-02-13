cb224400b4406d8f60c358d6c02177d1e5627d67
naleksander <aleksander.naszko@gmail.com>
2011-02-13 11:36:12 +0100

    Added :main to lein

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject textmash "1.0.0-SNAPSHOT"
   :description "TextMash - simple IDE for Clojure"
+  :main textmash.core
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
